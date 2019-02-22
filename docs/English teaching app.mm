f6c354ae36c1666d0938fb6b046177401de548ad
Jonathan Dyason <jonathandyason@hotmail.com>
2019-02-22 06:41:27 +0200

    next

---
+++ docs/English teaching app.mm
@@ -0,0 +1,14 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1545370755496" ID="ID_260440863" MODIFIED="1547229632370" TEXT="English teaching app">
+<node CREATED="1545370857082" HGAP="125" ID="ID_949433760" MODIFIED="1547229631637" POSITION="right" TEXT="extra feature" VSHIFT="196">
+<node CREATED="1545371048059" HGAP="252" ID="ID_1337404259" MODIFIED="1547229704495" TEXT="writing and storing teacher notes and grades on student" VSHIFT="91"/>
+</node>
+<node CREATED="1547229476567" HGAP="128" ID="ID_1403656183" MODIFIED="1547229498134" POSITION="left" TEXT="teacher can store data on what objectives the student has mastered" VSHIFT="-234"/>
+<node CREATED="1547229562646" HGAP="-616" ID="ID_1781860837" MODIFIED="1547229594768" POSITION="right" TEXT="and what objectives still need to be mastered" VSHIFT="-149"/>
+<node CREATED="1547229634336" HGAP="-115" ID="ID_134779633" MODIFIED="1547229650247" POSITION="left" TEXT="should include objectives of syllabus" VSHIFT="210"/>
+<node CREATED="1547229655325" HGAP="177" ID="ID_389344600" MODIFIED="1547229670216" POSITION="right" TEXT="should include check box UI" VSHIFT="-71"/>
+<node CREATED="1547229674838" HGAP="-424" ID="ID_453526419" MODIFIED="1547229697676" POSITION="left" TEXT="should include data storage regarding what is mastered or not" VSHIFT="-17"/>
+<node CREATED="1547386753703" HGAP="-511" ID="ID_662656169" MODIFIED="1547386773695" POSITION="right" TEXT="keeps teaching resources for teachers" VSHIFT="-15"/>
+</node>
+</map>
