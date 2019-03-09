d16d4e7eed99aef068fdb5b73ad58c49419b9bf1
Jonathan Dyason <jonathandyason@hotmail.com>
2019-03-09 16:32:52 +0200

    d

---
+++ script/script.js
@@ -0,0 +1,50 @@
+
+
+$("#submit1").click(function (e) {
+    if ($("#question").val() == 1) {
+        $('#result').text("correct").addClass('bg-success').removeClass('bg-danger');
+    }
+    else {
+        $('#result').text("incorrect").addClass('bg-danger').removeClass('bg-success');
+    }
+});
+
+
+$("#submit2").click(function (e) {
+    if ($("#question").val() == 3) {
+        $('#result').text("correct").addClass('bg-success').removeClass('bg-danger');
+    }
+    else {
+        $('#result').text("incorrect").addClass('bg-danger').removeClass('bg-success');
+    }
+});
+
+$("#submit3").click(function (e) {
+    if ($("#question").val() == 4) {
+        $('#result').text("correct").addClass('bg-success').removeClass('bg-danger');
+    }
+    else {
+        $('#result').text("incorrect").addClass('bg-danger').removeClass('bg-success');
+    }
+});
+
+
+
+$("#submit4").click(function (e) {
+    if ($("#question").val() == 3) {
+        $('#result').text("correct").addClass('bg-success').removeClass('bg-danger');
+    }
+    else {
+        $('#result').text("incorrect").addClass('bg-danger').removeClass('bg-success');
+    }
+});
+
+
+$("#submit5").click(function (e) {
+    if ($("#question").val() == 1) {
+        $('#result').text("correct").addClass('bg-success').removeClass('bg-danger');
+    }
+    else {
+        $('#result').text("incorrect").addClass('bg-danger').removeClass('bg-success');
+    }
+});
